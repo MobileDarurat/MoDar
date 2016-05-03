0c34e56e179decc5e55b5e7ede1cbedacda55aab
krisnasim <jonathan.simananda@outlook.com>
2016-05-03 15:57:19 +0700

    initial commmit

--- app/src/main/java/com/modar/modar/CustomGrid.java
+++ app/src/main/java/com/modar/modar/CustomGrid.java
@@ -5,6 +5,9 @@ package com.modar.modar;
  */
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,12 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class CustomGrid extends BaseAdapter{
+public class CustomGrid extends BaseAdapter {
     private Context mContext;
-    private final String[] init;
+    private final int[] init;
     private final String[] name;
 
-    public CustomGrid(Context c,String[] init,String[] name ) {
+    public CustomGrid(Context c, int[] init, String[] name) {
         mContext = c;
         this.init = init;
         this.name = name;
@@ -45,6 +48,7 @@ public class CustomGrid extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
         View grid;
+
         LayoutInflater inflater = (LayoutInflater) mContext
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -53,13 +57,45 @@ public class CustomGrid extends BaseAdapter{
             grid = new View(mContext);
             grid = inflater.inflate(R.layout.grid_single, null);
             TextView textView = (TextView) grid.findViewById(R.id.txtGrid);
-            Button btn = (Button)grid.findViewById(R.id.btnGrid);
+            Button btn = (Button) grid.findViewById(R.id.btnGrid);
+            //add some listener here
+            btn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Log.d("Modar", "PLEASE BISA DONK");
+                    Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                    phoneIntent.setData(Uri.parse("tel:087887978259"));
+//                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//                        // TODO: Consider calling
+//                        //    ActivityCompat#requestPermissions
+//                        // here to request the missing permissions, and then overriding
+//                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                        //                                          int[] grantResults)
+//                        // to handle the case where the user grants the permission. See the documentation
+//                        // for ActivityCompat#requestPermissions for more details.
+//
+//                        Log.d("Modar", "PLEASE BISA part 2");
+//
+//                        mContext.startActivity(phoneIntent);
+//                    }
+                    mContext.startActivity(phoneIntent);
+                }
+            });
+
             textView.setText(name[position]);
-            btn.setText(init[position]);
+            btn.setBackgroundResource(init[position]);
+            //btn.setText(init[position]);
             //imageView.setImageResource(Imageid[position]);
         } else {
             grid = (View) convertView;
         }
+//        grid.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("test","testdata");
+//            }
+//        });
 
         return grid;
     }
--- app/src/main/java/com/modar/modar/MainActivity.java
+++ app/src/main/java/com/modar/modar/MainActivity.java
@@ -2,49 +2,25 @@ package com.modar.modar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    GridView grid;
     String[] name = {
-            "Google",
-            "Github",
-            "Instagram",
-            "Facebook",
-            "Flickr",
-            "Pinterest",
-            "Quora",
-            "Twitter",
-            "Vimeo",
-            "WordPress",
-            "Youtube",
-            "Stumbleupon",
-            "SoundCloud",
-            "Reddit",
-            "Blogger"
-
+            "Ambulance",
+            "Derek",
+            "Pemadam Kebakaran",
+            "PAM",
+            "PLN",
+            "Polisi"
     } ;
-    String[] init = {
-            "Google",
-            "Github",
-            "Instagram",
-            "Facebook",
-            "Flickr",
-            "Pinterest",
-            "Quora",
-            "Twitter",
-            "Vimeo",
-            "WordPress",
-            "Youtube",
-            "Stumbleupon",
-            "SoundCloud",
-            "Reddit",
-            "Blogger"
-
+    int[] init = {
+            R.mipmap.ic_1,
+            R.mipmap.ic_2,
+            R.mipmap.ic_3,
+            R.mipmap.ic_4,
+            R.mipmap.ic_5,
+            R.mipmap.ic_6
     } ;
 
     @Override
@@ -52,19 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
         CustomGrid adapter = new CustomGrid(MainActivity.this, init, name);
-        grid=(GridView)findViewById(R.id.gridModar);
-        grid.setAdapter(adapter);
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        GridView gridViewModar = (GridView)findViewById(R.id.gridModar);
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "You Clicked at " + name[+position], Toast.LENGTH_SHORT).show();
+        gridViewModar.setAdapter(adapter);
 
-            }
-        });
+//        gridViewModar.setOnItemClickListener(new OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Log.d("Modar", "HAYO LOH KELUAR!");
+//                Toast.makeText(MainActivity.this, "You Clicked at " + init[position], Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+
     }
-
-
 }
